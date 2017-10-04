c81a1037a76cb416861b5d9d59665bcdce93980c
Animesh <hello@animesh.ltd>
2017-10-04 17:50:40 +0530

    Camera shows live feed now

--- camera.py
+++ camera.py
@@ -22,18 +22,37 @@ camera = cv2.VideoCapture(0)
 # rate, it always returns 0. So we need to either make an assumption or measure
 # frame reate using a timer. Latter approach is better.
 fps = 30
-
 frameHeight = int(camera.get(cv2.CAP_PROP_FRAME_HEIGHT))
 frameWidth  = int(camera.get(cv2.CAP_PROP_FRAME_WIDTH))
 frameSize   = (frameWidth, frameHeight)
 
-codec = cv2.VideoWriter_fourcc(*"I420")
-videoWriter = cv2.VideoWriter("assets/Capture.avi", codec, fps, frameSize)
-
 # Capture and save a 10 second video from camera
-success, frame = camera.read()
-framesRemaining = (10 * fps) - 1
-while success and framesRemaining > 0:
+def record():
+    codec = cv2.VideoWriter_fourcc(*"I420")
+    videoWriter = cv2.VideoWriter("assets/Capture.avi", codec, fps, frameSize) 
+    success, frame = camera.read()
+    framesRemaining = (10 * fps) - 1
+    while success and framesRemaining > 0:
         videoWriter.write(frame)
         success, frame = camera.read()
         framesRemaining -= 1
+
+
+# Mouse event handler
+clicked = False
+def onMouse(event, x, y, flags, param):
+    global clicked
+    if event == cv2.EVENT_LBUTTONUP:
+        clicked = True
+
+# Display live feed from the camera
+def showLiveFeed():
+    cv2.namedWindow("Occu.py")
+    cv2.setMouseCallback("Occu.py", onMouse)
+    success, frame = camera.read()
+    while success and cv2.waitKey(1) == -1 and not clicked:
+            cv2.imshow("Occu.py", frame)
+            success, frame = camera.read()
+    cv2.destroyWindow("Occu.py")
+
+showLiveFeed()
