42c4efba7827933fa294af31d76f30a644a17b9e
Animesh <hello@animesh.ltd>
2017-10-04 15:14:34 +0530

    Added raw bytes example to image.py

--- image.py
+++ image.py
@@ -14,6 +14,8 @@
 ##
 
 import cv2
+import numpy
+import os
 from enum import IntEnum
 
 # imread() uses flags to specify reading mode. They work just fine but
@@ -21,9 +23,9 @@ from enum import IntEnum
 # flags are simply numbers in disguise so I have created a convenience
 # type to make the code more readable.
 class ReadMode(IntEnum):
-    Colour = 1
-    Grayscale = 0
-    Unchanged = -1
+    Colour      = 1
+    Grayscale   = 0
+    Unchanged   = -1
 
 # Read an image on disk. By default imread() returns an image in BGR colour
 # format, even if the file is grayscale. BGR is same as RGB but the byte
@@ -42,3 +44,32 @@ if key == 27:
 elif key == ord("s"):
     cv2.imwrite("GrayUntitled.png", grayImage)
     cv2.destroyAllWindows()
+
+##
+##  Image and raw bytes
+##  
+##  An OpenCV image is a 2D or 3D array of type numpy.array. A grayscale image is
+##  a 2D array containing byte values (integers from 0 to 255) and a 24-bit BGR 
+##  image is a 3D array. We can access the individual pixels like so:
+##
+##  Grayscale   - image[y, x]
+##  BGR         - image[y, x, channel]
+##  E.g. image[0, 0] is the pixel at top-left corner of the image. If it's value
+##  is 255, it means it's a white pixel.
+##
+##  We can cast an image to a standard Python bytearray, provided it has 8 bits 
+##  per channel. Conversely, provided a bytearray contains bytes in an appropriate 
+##  order we can cast and then reshape it to get a numpy.array type that is an image.
+##
+
+# Make an array of 120,000 random bytes
+randomByteArray = bytearray(os.urandom(120000))
+flatNumpyArray = numpy.array(randomByteArray)
+
+# Convert the array to make a 400 x 300 grayscale image
+randomGrayImage = flatNumpyArray.reshape(300, 400)
+cv2.imwrite("RandomGray.png", randomGrayImage)
+
+# Convert the array to make a 400 x 100 colour image
+randomBGRImage = flatNumpyArray.reshape(100, 400, 3)
+cv2.imwrite("RandomBGR.png", randomBGRImage)
