9e29fa200fe2e3704ea5861791b76f761da5e55c
Animesh <hello@animesh.ltd>
2017-10-05 15:34:46 +0530

    Added WindowManager

---
+++ WindowManager.py
@@ -0,0 +1,44 @@
+##
+##  CaptureManager.py
+##  Occu.py
+##
+##  Created on October 4, 2017 by Animesh Mishra
+##  Copyright (c) 2017 Animesh Ltd. All Rights Reserved
+##
+
+class WindowManager(object):
+    """
+    A high-level interface for managing preview windows and keyboard support.
+    """
+
+    def __init__(self, windowName = "Occu.py", keypressCallback = None):
+        """
+        Args:
+            windowName (str): Name of the application window
+            keypressCallback (func): Event handler for key presses. Must take a single argument, an ASCII keycode.
+        """
+        self.keypressCallback = keypressCallback
+        self._windowName = windowName
+        self._isWindowCreated = False
+
+    @property
+    def isWindowCreated(self):
+        return self._isWindowCreated
+
+    def createWindow(self):
+        cv2.namedWindow(self._windowName)
+        self._isWindowCreated = True
+    
+    def show(self, frame):
+        cv2.imshow(self._windowName, frame)
+
+    def destroyWindow(self):
+        cv2.destroyWindow(self._windowName)
+        self._isWindowCreated = False
+
+    def processEvents(self):
+        keycode = cv2.waitKey(1)
+        if self.keypressCallback is not None and keycode != -1:
+            # Discard any non-ASCII info encoded by GTK
+            keycode &= 0xFF
+            self.keypressCallback(keycode)
